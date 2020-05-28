470d10700e6982cdc3861d431df0d7e4497c9887
firmansg <firman2008@ymail.com>
2020-05-28 11:05:16 +0700

    v.1

---
+++ js/script.js
@@ -0,0 +1,11 @@
+//Pagesroll
+$('.page-scroll').on('click', function () {
+
+    var tujuan = $(this).attr('href');
+
+    var elemenTujuan = $(tujuan);
+
+    $('html,body').animate({
+        scrollTop: elemenTujuan.offset().top
+    }, 1000);
+});
